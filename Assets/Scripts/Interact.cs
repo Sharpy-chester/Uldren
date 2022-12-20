64197930d2727d98a030db8ed207859d450f08df
Sharpy <cam-chester@hotmail.co.uk>
2022-12-20 05:40:03 +0000

    Adding assets

---
+++ Assets/Scripts/Interact.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interact : MonoBehaviour
+{
+    private void OnCollisionStay(Collision collision)
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+
+        }
+    }
+}
