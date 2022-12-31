27d01c15364174d90a7984d34eed553d5898722f
Sharpy <cam-chester@hotmail.co.uk>
2022-12-31 01:22:34 +0000

    Adding enemy stuff

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -8,20 +8,59 @@ public class EnemyAI : MonoBehaviour
 {
     public GameObject player;
     public NavMeshAgent navMeshAgent;
-    public float attackRange = 2f;
+    Animator animator;
+    [SerializeField] float attackRange = 2f;
+    [SerializeField] float aggroRange = 10f;
+    Rigidbody rb;
+    Vector3 lookPos;
+    int enemyLayerMask;
+    int raycastLayerMask;
+
+
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        animator.SetBool("Aggro", true);
+        lookPos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
+        enemyLayerMask = 1 << LayerMask.NameToLayer("Enemy");
+        raycastLayerMask = ~enemyLayerMask;
+    }
 
     void Update()
     {
-        navMeshAgent.SetDestination(player.transform.position);
-        float distance = Vector3.Distance(transform.position, player.transform.position);
-        if (distance < attackRange)
+        lookPos = new Vector3(transform.position.x, transform.position.y + 2, transform.position.z);
+        Ray ray = new(lookPos, player.transform.position - lookPos);
+        Debug.DrawLine(lookPos, player.transform.position, Color.green);
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, raycastLayerMask))
         {
-            Attack();
+            if (hit.transform.CompareTag("Player") && Vector3.Distance(lookPos, hit.transform.position) < aggroRange)
+            {
+                
+                animator.SetBool("SeePlayer", true);
+                float distance = Vector3.Distance(lookPos, player.transform.position);
+                if (distance < attackRange)
+                {
+                    navMeshAgent.ResetPath();
+                    Attack();
+                }
+                else if (distance < aggroRange)
+                {
+                    navMeshAgent.SetDestination(player.transform.position);
+                }
+            }
+            else
+            {
+                animator.SetBool("SeePlayer", false);
+            }
         }
+        animator.SetFloat("Speed", navMeshAgent.velocity.magnitude);
+        print(navMeshAgent.velocity.magnitude);
     }
 
     void Attack()
     {
         //Play anims and damage player
+        animator.SetTrigger("Attack");
     }
 }
--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Health : MonoBehaviour
 {
@@ -19,12 +20,28 @@ public class Health : MonoBehaviour
         {
             //kill
             print(gameObject.name + " died");
+            if (GetComponent<EnemyAI>())
+            {
+                Destroy(GetComponent<EnemyAI>());
+                Destroy(GetComponent<NavMeshAgent>());
+                GetComponent<Animator>().SetTrigger("Dead");
+                Destroy(GetComponent<BoxCollider>());
+                Destroy(this);
+            }
+        }
+        else
+        {
+            if (GetComponent<EnemyAI>())
+            {
+                GetComponent<Animator>().SetTrigger("Hit");
+            }
         }
     }
 
     public void DealDamage(float damage)
     {
         health -= damage;
+        
         CheckHealth();
     }
 
