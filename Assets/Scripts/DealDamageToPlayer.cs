5ae0974e10c873b4d62289b2db14d073ca8bf600
Sharpy <cam-chester@hotmail.co.uk>
2023-01-09 13:26:54 +0000

    Fixing issues with AI

--- Assets/Scripts/DealDamageToPlayer.cs
+++ Assets/Scripts/DealDamageToPlayer.cs
@@ -5,14 +5,32 @@ using UnityEngine;
 public class DealDamageToPlayer : MonoBehaviour
 {
     [SerializeField] int damage = 2;
+    bool hit;
+    BoxCollider box;
 
-    private void OnTriggerEnter(Collider other)
+    private void Start()
     {
-        if (other.GetComponent<ThirdPersonController>())
+        box = GetComponent<BoxCollider>();
+    }
+
+    void Update()
+    {
+        if(box.enabled == false)
+        {
+            hit = false;
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        print(other.transform.gameObject.name);
+        if (other.GetComponent<ThirdPersonController>() && !hit)
         {
             Health health = other.GetComponent<Health>();
             if (health != null)
             {
+                
+                hit = true;
                 health.DealDamage(damage);
             }
         }
